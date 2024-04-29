5c76dcc57b31ef08c186e4a46e510fc7481de057
Victor Mesquitta <mvictormesquita@gmail.com>
2024-04-29 15:27:41 -0300

    configuração inicial

---
+++ cadastro_veiculos/src/main/java/br/com/ecourbis/cadastro_veiculos/CadastroVeiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ecourbis.cadastro_veiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroVeiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroVeiculosApplication.class, args);
+	}
+
+}
---
+++ cadastro_veiculos/src/test/java/br/com/ecourbis/cadastro_veiculos/CadastroVeiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ecourbis.cadastro_veiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroVeiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
